15211d3d6581f32d38bfe571a2946fac03c3cd34
siff6 <hashifjade907@gmail.com>
2025-12-07 10:24:12 +0700

    SHA 3 Utility function + offset, FSM proccess

--- SHA3_256_Core.vhd
+++ SHA3_256_Core.vhd
@@ -58,3 +58,131 @@ architecture Behavioral of sha3_256_core is
         end if;
         return result;
     end function;
+        
+begin
+
+    process(clk, reset)
+        variable temp_state : lane_type;
+        type temp_array is array (0 to 4) of STD_LOGIC_VECTOR(63 downto 0);
+        variable C : temp_array;
+        variable D : temp_array;
+        variable B : lane_type;
+        variable x, y : integer;
+    begin
+        if reset = '1' then
+            current_state <= S_IDLE;
+            round_counter <= 0;
+            ready <= '1';
+            done <= '0';
+            hash_out <= (others => '0');
+            
+            for x in 0 to 4 loop
+                for y in 0 to 4 loop
+                    keccak_state(x, y) <= (others => '0');
+                end loop;
+            end loop;
+            
+        elsif rising_edge(clk) then
+            
+            case current_state is
+                
+                when S_IDLE =>
+                    ready <= '1';
+                    done <= '0';
+                    
+                    if start = '1' then
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                keccak_state(x, y) <= (others => '0');
+                            end loop;
+                        end loop;
+                        current_state <= S_ABSORB;
+                        ready <= '0';
+                    end if;
+                
+                when S_ABSORB =>
+                    keccak_state(0, 0) <= x"0000000000000006" xor (x"000000000000" & message_in);
+                    keccak_state(4, 3) <= x"8000000000000000";
+                    round_counter <= 0;
+                    current_state <= S_PERMUTE;
+                
+                when S_PERMUTE =>
+                    if round_counter < NUM_ROUNDS then
+                        temp_state := keccak_state;
+                        
+                        for x in 0 to 4 loop
+                            C(x) := temp_state(x,0) xor temp_state(x,1) xor 
+                                    temp_state(x,2) xor temp_state(x,3) xor 
+                                    temp_state(x,4);
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            D(x) := C((x+4) mod 5) xor rotate_left(C((x+1) mod 5), 1);
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                temp_state(x,y) := temp_state(x,y) xor D(x);
+                            end loop;
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                temp_state(x,y) := rotate_left(temp_state(x,y), RHO_OFFSETS(x,y));
+                            end loop;
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                B(x,y) := temp_state(x,y);
+                            end loop;
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                temp_state(y, (2*x + 3*y) mod 5) := B(x, y);
+                            end loop;
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                B(x,y) := temp_state(x,y);
+                            end loop;
+                        end loop;
+                        
+                        for x in 0 to 4 loop
+                            for y in 0 to 4 loop
+                                temp_state(x,y) := B(x,y) xor 
+                                    ((not B((x+1) mod 5, y)) and B((x+2) mod 5, y));
+                            end loop;
+                        end loop;
+                        
+                        temp_state(0,0) := temp_state(0,0) xor ROUND_CONSTANTS(round_counter);
+                        
+                        keccak_state <= temp_state;
+                        round_counter <= round_counter + 1;
+                        
+                    else
+                        current_state <= S_SQUEEZE;
+                    end if;
+                
+                when S_SQUEEZE =>
+                    hash_buffer(63 downto 0)     <= keccak_state(0, 0);
+                    hash_buffer(127 downto 64)   <= keccak_state(1, 0);
+                    hash_buffer(191 downto 128)  <= keccak_state(2, 0);
+                    hash_buffer(255 downto 192)  <= keccak_state(3, 0);
+                    current_state <= S_DONE;
+                
+                when S_DONE =>
+                    hash_out <= hash_buffer;
+                    done <= '1';
+                    ready <= '0';
+                    if start = '0' then
+                        current_state <= S_IDLE;
+                    end if;
+                    
+            end case;
+        end if;
+    end process;
+
+end Behavioral;
